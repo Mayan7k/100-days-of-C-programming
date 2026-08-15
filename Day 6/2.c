74289f62dfc13608494dee1087c07766136b2c6e
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-08-15 16:30:23 +0530

    Update 2.c

--- Day 6/2.c
+++ Day 6/2.c
@@ -1,1 +1,19 @@
+//Q12: Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
 
+#include<stdio.h>
+int main(){
+int n;
+  scanf("%d",&n);
+  if(n>=0){
+    if(n>0){
+      printf(" positive");
+    }else{
+      printf("zero");
+    }
+    
+  }
+else{
+  printf("negative");
+}
+  
+}
