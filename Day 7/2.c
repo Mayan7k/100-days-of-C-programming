786322192d0c9c93d815b0010afdc5625a232a90
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-08-16 16:49:38 +0530

    Update 2.c

--- Day 7/2.c
+++ Day 7/2.c
@@ -1,1 +1,15 @@
+//Write a program to input a character and check whether it is a vowel or consonant using if–else.
+#include <stdio.h>
 
+int main(){
+    char alphabet;
+    scanf("%c",&alphabet);
+    if (alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u')
+    {
+        printf("vowel");
+    }else{
+        printf("consonant");
+    }
+    
+    return 0;
+}
