954518f6a686b5968582c12bd816c6031b699626
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-08-16 16:44:22 +0530

    Update 1.c

--- Day 7/1.c
+++ Day 7/1.c
@@ -1,1 +1,15 @@
+//Write a program to input a year and check whether it is a leap year or not using conditional statements.
+#include <stdio.h>
 
+int main(){
+    int n;
+    scanf("%d",&n);
+   if (n%400==0 ||( n%4==0 && n%100!=0))
+   {
+   printf("leap yr");
+   }else{
+    printf("not a leap yr");
+   }
+   
+    return 0;
+}
