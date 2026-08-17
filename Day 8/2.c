77ac2ac75d02d17a2d8473cc8c6095a566821740
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-08-17 07:12:14 +0530

    Update 2.c

--- Day 8/2.c
+++ Day 8/2.c
@@ -1,1 +1,25 @@
+//Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+#include <stdio.h>
 
+int main(){
+        int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+if (a>b)
+{
+    if (a>c)
+    {
+        printf("a is largest");
+    }else{
+        printf("c is largest");
+    }
+    
+}else{
+    if (b>c)
+    {
+        printf("b is largest");
+    }else{
+        printf("c is largest");
+    }
+}
+    return 0;
+}
