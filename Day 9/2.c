56441c026f9642932d82464fae2f0477ecf072bc
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-08-28 19:35:43 +0530

    Update 2.c

--- Day 9/2.c
+++ Day 9/2.c
@@ -1,1 +1,23 @@
+//Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+#include <stdio.h>
 
+int main(){
+  int grade;
+  scanf("%d",&grade);
+if (grade>=90)
+{printf("Grade A ");
+}else if(grade>=80){
+  printf("Grade B ");
+}else if(grade >=70){
+  printf("Grade C");
+}else if (grade>=60)
+{
+  printf("Grade d ");
+}else{
+  printf("Grade F ");
+}
+
+
+  
+  return 0;
+}
