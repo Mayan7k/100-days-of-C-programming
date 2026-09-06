95eb2cf6badd6321c3dbef01bb8f23ebb49b2fea
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-06 10:29:18 +0530

    Update 1.c

--- Day 14/1.c
+++ Day 14/1.c
@@ -1,1 +1,14 @@
+//Write a program to print the sum of the first n odd numbers.
+#include <stdio.h>
 
+int main(){
+    int n;
+    int sum=0;
+    scanf("%d",&n);
+    for (int i = 1; i <=2*n-1; i=i+2)
+    {
+       sum = sum+i;
+    }printf("%d",sum);
+    
+    return 0;
+}   
