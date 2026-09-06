d40877e6f1182910a6ff04dfa6dd0a044bdf7a34
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-06 07:59:35 +0530

    Update 1.c

--- Day 13/1.c
+++ Day 13/1.c
@@ -1,1 +1,52 @@
 
+#include <stdio.h>
+
+int main() {
+    int num1, num2, result;
+    char op;
+
+    scanf("%d %d %c", &num1, &num2, &op); 
+
+    
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            printf("%d\n", result);
+            break;
+            
+        case '-':
+            result = num1 - num2;
+            printf("%d\n", result);
+            break;
+            
+        case '*':
+            result = num1 * num2;
+            printf("%d\n", result);
+            break;
+            
+        case '/':
+           
+            if (num2 == 0) {
+                printf("Error: Division by zero\n");
+            } else {
+                result = num1 / num2;
+                printf("%d\n", result);
+            }
+            break;
+            
+        case '%':
+            if (num2 == 0) {
+                printf("Error: Modulo by zero\n");
+            } else {
+                result = num1 % num2;
+                printf("%d\n", result);
+            }
+            break;
+            
+        default:
+            printf("Error: Invalid operator\n");
+            break;
+    }
+
+    return 0;
+}
