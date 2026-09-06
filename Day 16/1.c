aa9cf72e58be1127a8f0838a41c2cfb85d60d3cd
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-06 11:58:15 +0530

    Update 1.c

--- Day 16/1.c
+++ Day 16/1.c
@@ -1,1 +1,10 @@
+//Write a program to take a number as input and print its equivalent binary representation.
+#include <stdio.h>
 
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("%04b",n);
+    
+    return 0;
+}
