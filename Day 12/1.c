849e76d3ea03cc52ce45109534a4085658107559
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-06 07:53:03 +0530

    Update 1.c

--- Day 12/1.c
+++ Day 12/1.c
@@ -1,1 +1,26 @@
+/*Write a program to calculate library fine based on late days as follows: 
+First 5 days late: ₹2/day 
+Next 5 days late: ₹4/day 
+Next 20 days days late: ₹6/day 
+More than 30 days: Membership Cancelled.*/
+  #include <stdio.h>
 
+int main(){
+    int days,fine;
+    scanf("%d",&days);
+    if (days<=5)
+    {
+        printf("%d",fine = 2*days);
+    }else if ( days<=10)
+    {
+       printf("%d",fine = 10+(4*(days-5)));
+    }else if ( days<=30)
+    {
+         printf("%d",fine = 10+20+(6*(days-10)));
+    }else {
+    printf("membership cancelled");
+    }
+    
+    
+    return 0;
+}
