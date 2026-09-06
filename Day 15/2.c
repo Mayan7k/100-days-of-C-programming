56aa2df7b49e36be2c2348b86b96a249c472874b
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-06 11:42:55 +0530

    Update 2.c

--- Day 15/2.c
+++ Day 15/2.c
@@ -1,1 +1,14 @@
+//Write a program to reverse a given number.
+#include <stdio.h>
 
+int main(){
+    int n;
+    scanf("%d",&n);
+    int revNum=0;
+    for (; n!=0; n = n/10){
+       int ld=n%10;
+       revNum=(revNum*10)+ld;
+    }
+    printf("%d",revNum);
+    return 0;
+}
