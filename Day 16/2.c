e826f730a3f6592721364ffde8c8618012785867
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-06 12:07:52 +0530

    Update 2.c

--- Day 16/2.c
+++ Day 16/2.c
@@ -1,1 +1,24 @@
-
+//Write a program to check if a number is a palindrome.
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int originalNum=n;
+    int revNum=0;
+    for (; n!=0; n=n/10)
+    {
+        int ld= n%10;
+        revNum=(revNum*10)+ld ;
+    } 
+    if (revNum==originalNum)
+    {
+        printf("it is a palindrome");
+    }else
+    {
+        printf("it is not a palindrome");
+    }
+    
+    
+    
+    return 0;
+}
