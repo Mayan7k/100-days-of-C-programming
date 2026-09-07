8b66b44f58f1ccdb42e83160573cc4c0bab97d79
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-07 17:48:32 +0530

    Add Armstrong number checker in 1.c
    Implement a program to check for Armstrong numbers.

--- Day 17/1.c
+++ Day 17/1.c
@@ -1,1 +1,22 @@
+//Write a program to check if a number is an Armstrong number.
+#include <stdio.h>
 
+int main(){
+  int n,ld;
+  scanf("%d",&n);
+  int original =n;
+  int sum=0;
+  for ( ; n!=0; n=n/10)
+  {
+     ld=n%10;
+     sum=sum + (ld)*(ld)*(ld);
+  } if (original==sum)
+  {
+    printf("the number is armstrong number");
+  }else {
+    printf("the number is not an armstrong number");
+  }
+  
+  
+  return 0;
+}
