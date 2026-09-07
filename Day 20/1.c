327519e94c406a549399b098f066113659ecb6b9
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-07 20:14:19 +0530

    Update 1.c

--- Day 20/1.c
+++ Day 20/1.c
@@ -1,1 +1,19 @@
+//Write a program to find the product of odd digits of a number
+#include <stdio.h>
 
+int main(){
+  int n;
+  scanf("%d",&n);
+  int product=1;
+  for (  ;n!=0; n=n/10)
+  {
+    int ld=n%10;
+    if (ld%2==1)
+    {
+      product=product*ld;
+    }
+    
+  }
+  printf("%d",product);
+  return 0;
+}
