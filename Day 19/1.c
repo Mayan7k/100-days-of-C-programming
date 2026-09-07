015414fe93fe70a489ae85a50a88d5f4edcfe5fc
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-07 19:52:20 +0530

    Update 1.c

--- Day 19/1.c
+++ Day 19/1.c
@@ -1,1 +1,18 @@
+//Write a program to find the LCM of two numbers.
+#include <stdio.h>
 
+int main()
+{
+    int n1,n2;
+    scanf("%d %d", &n1,&n2); 
+      int product=(n1*n2);
+   while (n2 >0)
+    {    
+ int rem= n1%n2;
+        n1=n2;
+        n2=rem; 
+  }
+int LCM = product/n1 ;
+  printf("the LCM IS %d",LCM);
+    return 0;
+}
