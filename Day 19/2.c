5e485e7da40224224c663b15f15f00c0d07d671e
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-07 20:02:27 +0530

    Update 2.c

--- Day 19/2.c
+++ Day 19/2.c
@@ -1,1 +1,15 @@
+//Write a program to find the sum of digits of a number.
+#include <stdio.h>
 
+int main(){
+  int n;
+  int sum=0;
+  scanf("%d",&n);
+  for (; n!=0; n=n/10)
+  {
+    int ld=n%10;
+    sum=sum+ld;
+  }
+  printf("%d",sum);
+  return 0;
+}
