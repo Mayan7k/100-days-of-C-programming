82c5d37d4b0c96d128d93f3b722a57a0000c6fa0
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-07 19:35:52 +0530

    Update 2.c

--- Day 18/2.c
+++ Day 18/2.c
@@ -1,1 +1,24 @@
+//Write a program to find the HCF (GCD) of two numbers.
+#include <stdio.h>
 
+int main()
+{
+    int n1,n2;
+
+    scanf("%d %d", &n1,&n2); 
+   while (n2 >0)
+    {
+     
+      if (n1/n2 !=0)
+      { int rem= n1%n2;
+        n1=n2;
+        n2=rem;
+        
+      
+    }
+     
+  }
+printf("the hcf is %d",n2);
+  
+    return 0;
+}
