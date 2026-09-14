af95fdb1ca6770b26121b7ab7235f2d221cc5dea
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-14 08:36:41 +0530

    Update 1.c

--- Day 21/1.c
+++ Day 21/1.c
@@ -1,1 +1,15 @@
+//Write a program to swap the first and last digit of a number.
+#include <stdio.h>
 
+int main(){
+    int n;
+    int fd;
+    int ld;
+    scanf("%d",&n);
+   ld=n%10;
+   int n1=n/1000;
+   int n2=(n%1000)/10;
+   int new=ld*1000 + n2*10 + n1 ;
+   printf("new= %d ", new);
+    return 0;
+}
