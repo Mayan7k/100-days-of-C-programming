22dff18462ad980d009b6c6286a0618b5898a4ab
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-14 19:22:09 +0530

    Update 1.c

--- Day 23/1.c
+++ Day 23/1.c
@@ -1,1 +1,15 @@
 
+//Write a program to find the sum of the series:2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+#include <stdio.h>
+
+int main(){
+    int n;
+    float sum=0.0;
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+  sum= sum + (2.0*i)/((4.0*i)-1);
+    }
+    printf("%.1f",sum);
+    return 0;
+}
+
