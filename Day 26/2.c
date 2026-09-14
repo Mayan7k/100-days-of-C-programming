70b40d5032d3882a09b6153bbfb3055284fd879f
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-14 22:24:32 +0530

    Update 2.c

--- Day 26/2.c
+++ Day 26/2.c
@@ -1,1 +1,48 @@
+/*Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+*/
+#include <stdio.h>
+
+int main() {
+    int n = 5; 
+
+    
+    for (int i = 1; i <= 3; i++) {
+        int stars = 2 * i - 1; 
+        
+        for (int j = 1; j <= stars; j++) {
+            printf("*\n");
+        }
+        printf("\n"); 
+    }
+
+    
+    for (int i = 2; i >= 1; i--) {
+        int stars = 2 * i - 1; 
+        
+        for (int j = 1; j <= stars; j++) {
+            printf("*\n");
+        }
+        printf("\n"); 
+    }
+
+    return 0;
+}
 
