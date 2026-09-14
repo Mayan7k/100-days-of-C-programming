5517300c1320ec659769cffa6ee0c29d99b7eaa5
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-14 17:37:59 +0530

    Update 2.c

--- Day 22/2.c
+++ Day 22/2.c
@@ -1,3 +1,4 @@
+
 //Write a program to find the sum of the series: 1 + 3/4 + 5/6 + 7/8 + … up to n terms.
 #include <stdio.h>
 
@@ -5,9 +6,9 @@ int main(){
     int n;
     float sum=1.0;
     scanf("%d",&n);
-    for(int i=1;i<=n;i++){
-  sum= sum + (2.0*i+1.0)/(2.0+2.0*i);
+    for(int i=2;i<=n;i++){
+  sum= sum + (2.0*i-1.0)/(2.0*i);
     }
-    printf("%.1f",formula);
+    printf("%.1f",sum);
     return 0;
 }
