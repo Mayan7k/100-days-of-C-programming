f2e2b87969c1d44d7e7474b5053dba819c4134a3
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-14 19:55:35 +0530

    Update 1.c

--- Day 26/1.c
+++ Day 26/1.c
@@ -1,1 +1,26 @@
+/*Write a program to print the following pattern:
+    5
+   45
+  345
+ 2345
+12345 */
+#include <stdio.h>
 
+int main(){
+  int n=5;
+  
+  for (int i = 5; i > 0; i--)
+  {   for (int space = 1; space <=i-1; space++)
+    {
+      printf(" ",space);
+    }
+    for (int j = i; j<=n; j++)
+    {
+      printf("%d",j);
+    }
+    printf("\n");
+  }
+  
+
+  return 0;
+}
