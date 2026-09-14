c765b9f6e78a8b30373e055431432129f1f68729
Mayank Mishra <Mayank.38054@stu.upes.ac.in>
2026-09-14 08:45:45 +0530

    Update 2.c

--- Day 21/2.c
+++ Day 21/2.c
@@ -1,1 +1,18 @@
+//Write a program to check if a number is a perfect number.
+#include <stdio.h>
 
+int main(){
+    int n;
+    int sum=0;
+    scanf("%d",&n);
+   for(int i=1;i<n;i++){
+    if(n%i==0)
+    sum=sum+i;
+}
+    if(sum==n)
+    printf("perfect number");
+   else
+   printf(" not a perfect number");
+   
+    return 0;
+}
